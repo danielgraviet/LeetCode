67a95951841f8674941eae0aaff46856c5007567
danielgraviet <dgravs5@gmail.com>
2024-12-03 20:18:01 -0700

    added fibonacci practice

--- dfs_tree/fibonacci.py
+++ dfs_tree/fibonacci.py
@@ -1,21 +1,20 @@
 class Solution(object):
-    def fib(self, num):
+    def fibonacci(self, num):
         if num < 0:
-            return "Error"
+            raise ValueError
         if num == 1:
             return 1
         
         a,b = 1, 1
         for _ in range(2, num):
             a, b = b, a + b
-        
+            
         return b
-        
         
 def main():
     solution = Solution()
-    num = 7
-    print(solution.fib(num))
+    print(solution.fibonacci(7))
+    
 
 if __name__ == "__main__":
     main()
