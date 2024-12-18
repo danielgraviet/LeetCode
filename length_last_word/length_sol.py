2cfd617cebfc20c7a1f96e6b60334f49e2c38492
danielgraviet <dgravs5@gmail.com>
2024-12-17 18:01:05 -0700

    pythonic solution for length of last word

--- length_last_word/length_sol.py
+++ length_last_word/length_sol.py
@@ -12,11 +12,32 @@ class Solution(object):
             length += 1
             i -= 1
         return length
-        
+    
+    def pythonicSolution(self, s):
+        #strip the string of leading/trailing spaces
+        s.strip()
+        if s.strip():
+            stringList = s.split()
+            return len(stringList[-1])
+        else:
+            return 0
 
 def main():
     solution = Solution()
-    print(solution.lengthOfLastWord("d"))
+    string = "  this is danny  "
+    print(solution.lengthOfLastWord(string))
+    print(solution.pythonicSolution(string))
+    
+    print(string.strip()) #this removes trailing 0's
+    if string.strip():
+        print("The string is not empty!")
+    else:
+        print("The string is empty.")
+
+    stringList = (string.split())
+    for word in stringList:
+        print(word)
+
 
 if __name__ == "__main__":
     main()
