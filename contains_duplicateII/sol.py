db3fe97c540bedf90db373ffe3ef1cc43e4b20e5
danielgraviet <dgravs5@gmail.com>
2024-12-29 21:12:16 -0700

    solved insert delete get random

--- contains_duplicateII/sol.py
+++ contains_duplicateII/sol.py
@@ -16,17 +16,5 @@ class Solution(object):
 def main():
     sol = Solution()
     print(sol.containsNearbyDuplicate([1,2,3,1,2,3], 2))
-
-    # test_dict = {1:[0, -3], 2:[1], 3:[2]}
-    # for key in test_dict:
-    #     if len(test_dict[key]) >= 2:
-    #         # subtract the values, get the abs.
-    #         print(abs(test_dict[key][0] + test_dict[key][1]))
-    #     else:
-    #         print(False)
-    array = [0,2,3]
-    print(array[-1])
-    print(array[-2])
-
 if __name__ == "__main__":
     main()
