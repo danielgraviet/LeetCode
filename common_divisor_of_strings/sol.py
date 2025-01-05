3c009d48caa16f8f5f2a2d4e4261939854d1bfb9
danielgraviet <dgravs5@gmail.com>
2025-01-04 17:18:02 -0700

    final solution for gcd of strings

--- common_divisor_of_strings/sol.py
+++ common_divisor_of_strings/sol.py
@@ -5,6 +5,9 @@ class Solution(object):
         :type str2: str
         :rtype: str
         """
+        smallStr = min(str1, str2)
+        bigStr = max(str1, str2)
+        print(bigStr)
         res = []
         if str1[0] != str2[0]:
             return ""
@@ -13,10 +16,23 @@ class Solution(object):
                 if firstChar == secondChar:
                     res.append(firstChar)
         return "".join(res)
+
+    def fastSolution(self, str1, str2):
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(a, b):
+            while b:
+                a,b = b, a % b
+            return a
+
+        gcd_string = (gcd(len(str1), len(str2)))
+        return str1[:gcd_string]
+
                 
 def main():
     sol = Solution()
-    print(sol.gcdOfStrings("ABCABC", "ABC"))
+    print(sol.fastSolution("ABCABC", "ABC"))
 
 if __name__ == "__main__":
     main()
