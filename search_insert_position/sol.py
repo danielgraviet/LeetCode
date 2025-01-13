1cbc7eb2fe242c58c2e1bddba215cca9f9c82fa0
danielgraviet <dgravs5@gmail.com>
2025-01-13 14:45:57 -0700

    practiced binary search

--- search_insert_position/sol.py
+++ search_insert_position/sol.py
@@ -15,12 +15,26 @@ class Solution(object):
             else:
                 left = middle + 1
         return left
-        
+    
+    def practice(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            if nums[middle] > target:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return left       
+     
 def main():
     sol = Solution()
     array = [1,2,4,5,7,8,40,90]
     target = 2
     print(sol.searchInsert(array, target))
+    print(sol.practice(array, target))
+    
     
 
 if __name__ == "__main__":
