15795d932f9f6ab21cc41b736e96bbecd06dcea0
danielgraviet <dgravs5@gmail.com>
2025-01-16 16:56:30 -0700

    started working on number to roman numeral problem

---
+++ number_to_numeral/sol.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        romanList = [["I", 1],
+                     ["IV", 4],
+                     ["V", 5],
+                     ["IX", 9]
+                     ["X", 10],
+                     ["XL", 40]
+                     ["L", 50],
+                     ["XC", 90],
+                     ["C", 100],
+                     ["CD", 400],
+                     ["D", 500],
+                     ["CM", 900]
+                     ["M", 1000]]
+        
+        
+
+    def extract_places(self, number):
+        place_values = []
+        place = 1
+        
+        while number > 0:
+            digit = number % 10
+            place_values.append(digit * place)
+            number //= 10
+            place *= 10
+
+        return place_values[::-1]
+
+        
+def main():
+    sol = Solution()
+    print(sol.intToRoman(3749))
+
+if __name__ == "__main__":
+    main()
