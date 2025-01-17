cc562af584f8c624be3c3fe0921275ac26c84b55
danielgraviet <dgravs5@gmail.com>
2025-01-16 18:46:39 -0700

    valid solution

--- number_to_numeral/sol.py
+++ number_to_numeral/sol.py
@@ -7,19 +7,26 @@ class Solution(object):
         romanList = [["I", 1],
                      ["IV", 4],
                      ["V", 5],
-                     ["IX", 9]
+                     ["IX", 9],
                      ["X", 10],
-                     ["XL", 40]
+                     ["XL", 40],
                      ["L", 50],
                      ["XC", 90],
                      ["C", 100],
                      ["CD", 400],
                      ["D", 500],
-                     ["CM", 900]
+                     ["CM", 900],
                      ["M", 1000]]
         
+        res = ""
         
-
+        for sym, digit in romanList[::-1]:
+            if num // digit:
+                count = num // digit
+                res += (count * sym)
+                num = num % digit
+        return res
+    
     def extract_places(self, number):
         place_values = []
         place = 1
