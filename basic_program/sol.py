1f75f490fa083907d3b97d1a9c07e9cf65f530fd
danielgraviet <dgravs5@gmail.com>
2025-02-04 17:16:57 -0700

    sliding window practice

---
+++ basic_program/sol.py
@@ -0,0 +1,36 @@
+def main():
+    age = 18
+    name = "Geoffrey"
+
+    if (age > 18): # this statement needs to be true
+        print(name) # to run this code. 
+
+    if (True):
+        print("This will always run")
+
+    if (name):
+        print("Will this run? ")
+
+
+    dishes = []
+    dishes.append("dish1")
+    dishes.append("dish2")
+    dishes.append("dish3")
+
+    users = {}
+    users[1] = "Danny"
+    users[2] = "Alex"
+    users[3] = "Caelum"
+
+    studentGrades = [99, 87, 79, 99, 100]
+    print(studentGrades[0])
+    print(studentGrades[1])
+    print(studentGrades[2])
+    print(studentGrades[3])
+    print(studentGrades[4])
+
+
+
+
+if __name__ == "__main__":
+    main()
--- sliding_window/sol.py
+++ sliding_window/sol.py
@@ -1,74 +1,24 @@
 class Solution(object):
-    def sliding_window(self, letters):
-        l = 0
-        longest = 0
-        n = len(letters)
-        sett = set()
-
+    def lengthOfLongestSubstring(self, s):
+        left, longest = 0, 0
+        n = len(s)
+        letters = set()
+        
         for r in range(n):
-            while letters[r] in sett:
-                sett.remove(letters[l])
-                l += 1
-                
-            w = (r-l) + 1
-            longest = max(longest, w)
-            sett.add(letters[r])
-
+            while s[r] in letters:
+                letters.remove(s[left])
+                left += 1
+            window = (r - left) + 1
+            letters.add(s[r])
+            longest = max(longest, window)
+            
         return longest
-    
-    def max_sum_subarray(self, arr, k):
-        # Your code here
-        curSub = sum(arr[:k])
-        maxSub = curSub
-
-        for i in range(k, len(arr)):
-            curSub += arr[i]
-            curSub -= arr[i-k]
-
-            maxSub = max(maxSub, curSub)
-        return maxSub
-    
-    def find_max_average(self, nums, k):
-        n = len(nums)
-        cur_sum = 0
-
-        for i in range(k):
-            cur_sum += nums[i]
         
-        max_avg = cur_sum / k
-
-        for i in range(k, n):
-            cur_sum += nums[i]
-            cur_sum -= nums[i - k]
-
-            avg = cur_sum / k
-            max_avg = max(max_avg, avg)
-        
-        return max_avg
-    
-    def maxSum(self, nums, k):
-        # find starting window. 
-        startSum = sum(nums[:k])
-        maxSum = startSum
-
-        # slide the window over, until the last spot
-        for i in range(k, len(nums)):
-            startSum += nums[i]
-            startSum -= nums[i - k]
-
-            maxSum = max(maxSum, startSum)
-        # keep track of the max. 
-        return maxSum
+            
 
 def main():
     sol = Solution()
-
-
-     # Test the function
-    arr = [1, 4, 2, 10, 23, 3, 1, 0, 20]
-    k = 4
-    result = sol.max_sum_subarray(arr, k)
-    print(f"Maximum sum of subarray of size {k}: {result}")
+    print(sol.lengthOfLongestSubstring('pwwek'))
 
 if __name__ == "__main__":
     main()
