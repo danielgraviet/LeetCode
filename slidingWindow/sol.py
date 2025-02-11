35d122d8a2ed4d2242585bc3349000c11aa16a2b
danielgraviet <dgravs5@gmail.com>
2025-02-10 20:05:40 -0700

    sliding window

--- slidingWindow/sol.py
+++ slidingWindow/sol.py
@@ -22,10 +22,30 @@ class Solution:
                 left += 1
                 
         return maxSum
+    
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if k > len(nums):
+            return 0
+        
+        windowSum = sum(nums[:k])
+        left = 0
+        maxWindow = windowSum / k
+        
+        for right in range(k, len(nums)):
+            windowSum = windowSum + nums[right] - nums[left]
+            windowAvg = windowSum / k
+            maxWindow = max(maxWindow, windowAvg)
+            left += 1
+        
+        return maxWindow
+        
+        
+    
         
 def main():
     sol = Solution()
     print(sol.maximumSubarraySum([1,2,2], 2))
+    print(sol.findMaxAverage([1,12,-5,-6,50,3], 4))
     
     
 if __name__ == "__main__":
