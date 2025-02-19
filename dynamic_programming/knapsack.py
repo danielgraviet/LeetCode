9f2133376245c707361a23ea56abd7e4bf7a7e62
danielgraviet <dgravs5@gmail.com>
2025-02-19 16:58:42 -0700

    more dynamic programming

---
+++ dynamic_programming/knapsack.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def knapsack(self, values, weights, capacity):
+        # check for edge cases
+        if len(values) != len(weights) or capacity < 0:
+            raise ValueError
+        
+        # init dp array and other variables
+        dp = [0] * (capacity + 1)
+        n = len(weights)
+        
+        # double for loop with i and w
+        for i in range(n):
+            for w in range(capacity, weights[i] - 1, -1):
+                dp[w] = max(dp[w], values[i] + dp[w - weights[i]])
+        
+        # return dp[capacity]
+        return dp[capacity]
+    
+def main():
+    sol = Solution()
+    
+    values = [1,3,4]
+    weights = [4,3,2]
+    capacity = 6
+    
+    print(sol.knapsack(values, weights, capacity))
+    
+if __name__ == "__main__":
+    main()
